3d338a0d5e5b394a0192eb50e4149dd34b35faec
Jorge Ribeiro <estertakeuchi@Esters-MacBook-Air.local>
2025-06-05 00:36:45 +0100

    fix maven version

--- healthservice-host/src/test/java/uk/co/mycompany/healthservice/HealhServiceApplicationTests.java
+++ healthservice-host/src/test/java/uk/co/mycompany/healthservice/HealhServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package uk.co.mycompany.healthservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootConfiguration
+@ContextConfiguration
 class HealhServiceApplicationTests {
 
 	@Test
